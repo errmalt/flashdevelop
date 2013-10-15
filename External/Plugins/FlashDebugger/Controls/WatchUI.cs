dc74d02a3d181c813c87fd9e7f2bfd74016cbd0c
Damjan Cvetko <damjan.cvetko@gmail.com>
2013-10-15 23:51:10 +0200

    Abstracted watchUI.

--- External/Plugins/FlashDebugger/Controls/WatchUI.cs
+++ External/Plugins/FlashDebugger/Controls/WatchUI.cs
@@ -66,11 +66,7 @@ namespace FlashDebugger.Controls
 				DataNode node = new DataNode(item); // todo, introduce new Node types.
 				try
 				{
-                    IASTBuilder builder = new ASTBuilder(false);
-                    ValueExp exp = builder.parse(new java.io.StringReader(item));
-                    var ctx = new ExpressionContext(PluginMain.debugManager.FlashInterface.Session, PluginMain.debugManager.FlashInterface.Session.getFrames()[PluginMain.debugManager.CurrentFrame]);
-                    var obj = exp.evaluate(ctx);
-                    node = new FlashDataNode((Variable)obj, item, PluginMain.debugManager.FlashInterface.Session);
+					node = PluginMain.debugManager.DebuggerInterface.GetExpressionNode(item);
 				}
 				catch { }
 				node.Text = item;
